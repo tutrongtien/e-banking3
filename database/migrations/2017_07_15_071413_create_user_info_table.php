5075c8c3ad2ce8be54491709718835d13f94c94d
Teo192000 <huynguyen192000@gmail.com>
2017-07-15 16:05:01 +0700

    T_T

---
+++ database/migrations/2017_07_15_071413_create_user_info_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_info', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('identity_card');
+            $table->date('date_of_identity_card');
+            $table->string('name', 100);
+            $table->date('date_of_birth');
+            $table->string('phone', 20);
+            $table->string('address', 100);
+            $table->string('city', 100);
+            $table->string('district', 100);
+            $table->string('ward', 100);
+            $table->string('email', 100);
+            $table->string('job', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_info');
+    }
+}
---
+++ database/migrations/2017_07_15_075042_add_foreign_key_user_table_on_user_info.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignKeyUserTableOnUserInfo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign('id')->references('id')->on('user_info')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('users_id_foreign');
+        });
+    }
+}
