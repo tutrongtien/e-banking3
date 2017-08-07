0820360e57b4bb38a93e1792d619c371b80a07f3
VutanDo <vutanqn@gmail.com>
2017-08-07 21:47:48 +0700

    view balance and transactions

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        // $this->call(BankAccountSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(BankAccountSeeder::class);
         $this->call(AccountsTableSeeder::class);
         $this->call(TransactionTableSeeder::class);
     }
